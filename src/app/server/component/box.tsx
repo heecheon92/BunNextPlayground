7b28a543c827a50bc13a24137fe777c59673aa17
Heecheon Park <heecheon92@gmail.com>
2023-09-29 14:10:51 +0900

    fixing misplaced text

--- src/app/server/component/box.tsx
+++ src/app/server/component/box.tsx
@@ -1,7 +1,7 @@
 import { isServerSide } from "@/app/util";
 
 export default function Box() {
-  console.log("is client side: ", isServerSide());
+  console.log("is server side: ", isServerSide());
 
   return <div className="h-[100px] w-[100px] bg-rose-500"></div>;
 }
